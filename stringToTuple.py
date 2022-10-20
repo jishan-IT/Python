75c955d6a671e1c5b8626a6ba676fad008f0c007
jishan-IT <kmfarjadullah@gmail.com>
2022-10-20 18:59:06 +0600

    string to tuple with python

---
+++ stringToTuple.py
@@ -0,0 +1,6 @@
+anomalWord = "faiza tasiba aleya abrar taskin ira taifa farjad hasnath zim shihab samil maruf "
+spliteword = anomalWord.split()
+converTuple = tuple(sorted(spliteword))
+lengthTuple = len(converTuple)
+print(converTuple)
+print(lengthTuple)
